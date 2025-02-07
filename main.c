a760012a42633cb4ff7870106b26dc9685cd70f9
Selin Avcı <selinavci2004@icloud.com>
2025-02-07 22:13:41 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  seyahat planlaması
+//  spor salonunda geçirilen süre hesaplanması
 //
 //  Created by Selin Avcı on 7.02.2025.
 //
@@ -9,56 +9,54 @@
 #include <time.h>
 
 // Tarih ve saat bilgisini saklamak için struct
-struct DateTime {
-    int year, month, day, hour, minute, second;
+struct TarihSaat {
+    int yil, ay, gun, saat, dakika, saniye;
 };
 
 // Struct'ı epoch zamanına çeviren fonksiyon
-time_t convertToEpoch(struct DateTime dt) {
-    struct tm timeinfo = {0};
-
-    timeinfo.tm_year = dt.year - 1900;  // Yıl 1900'den başlar
-    timeinfo.tm_mon = dt.month - 1;     // Aylar 0'dan başlar
-    timeinfo.tm_mday = dt.day;
-    timeinfo.tm_hour = dt.hour;
-    timeinfo.tm_min = dt.minute;
-    timeinfo.tm_sec = dt.second;
-
-    return mktime(&timeinfo);
+time_t tariheDonustur(struct TarihSaat ts) {
+    struct tm zamanBilgisi = {0};
+    
+    zamanBilgisi.tm_year = ts.yil - 1900;
+    zamanBilgisi.tm_mon = ts.ay - 1;
+    zamanBilgisi.tm_mday = ts.gun;
+    zamanBilgisi.tm_hour = ts.saat;
+    zamanBilgisi.tm_min = ts.dakika;
+    zamanBilgisi.tm_sec = ts.saniye;
+    
+    return mktime(&zamanBilgisi);
 }
 
 int main() {
-    struct DateTime departure, arrival;
-    time_t epoch_departure, epoch_arrival;
-    double seconds_diff;
-    int days, hours, minutes;
-
+    struct TarihSaat giris, cikis;
+    time_t epoch_giris, epoch_cikis;
+    double saniye_farki;
+    int saat, dakika;
+    
     // Kullanıcıdan giriş al
-    printf("Gidiş tarihini gir (YYYY MM DD HH MM SS): ");
-    scanf("%d %d %d %d %d %d", &departure.year, &departure.month, &departure.day,
-          &departure.hour, &departure.minute, &departure.second);
-
-    printf("Dönüş tarihini gir (YYYY MM DD HH MM SS): ");
-    scanf("%d %d %d %d %d %d", &arrival.year, &arrival.month, &arrival.day,
-          &arrival.hour, &arrival.minute, &arrival.second);
-
+    printf("Spor salonuna giriş tarihini gir (YYYY MM DD HH MM SS): ");
+    scanf("%d %d %d %d %d %d", &giris.yil, &giris.ay, &giris.gun,
+          &giris.saat, &giris.dakika, &giris.saniye);
+    
+    printf("Spor salonundan çıkış tarihini gir (YYYY MM DD HH MM SS): ");
+    scanf("%d %d %d %d %d %d", &cikis.yil, &cikis.ay, &cikis.gun,
+          &cikis.saat, &cikis.dakika, &cikis.saniye);
+    
     // Epoch zamanına çevir
-    epoch_departure = convertToEpoch(departure);
-    epoch_arrival = convertToEpoch(arrival);
-
+    epoch_giris = tariheDonustur(giris);
+    epoch_cikis = tariheDonustur(cikis);
+    
     // Zaman farkını hesapla
-    seconds_diff = difftime(epoch_arrival, epoch_departure);
-
-    days = seconds_diff / (24 * 3600);
-    seconds_diff = (int)seconds_diff % (24 * 3600);
-    hours = seconds_diff / 3600;
-    seconds_diff = (int)seconds_diff % 3600;
-    minutes = seconds_diff / 60;
-
+    saniye_farki = difftime(epoch_cikis, epoch_giris);
+    saat = saniye_farki / 3600;
+    saniye_farki = (int)saniye_farki % 3600;
+    dakika = saniye_farki / 60;
+    
     // Sonuçları ekrana yazdır
-    printf("\nSeyahat Süresi: %d gün, %d saat, %d dakika\n", days, hours, minutes);
-    printf("Gidiş Tarihi (Epoch): %ld\n", epoch_departure);
-    printf("Dönüş Tarihi (Epoch): %ld\n", epoch_arrival);
+    printf("\nSpor salonunda geçirilen süre: %d saat, %d dakika\n", saat, dakika);
+    printf("Giriş Zamanı (Epoch): %ld\n", epoch_giris);
+    printf("Çıkış Zamanı (Epoch): %ld\n", epoch_cikis);
+
 
     return 0;
 }
